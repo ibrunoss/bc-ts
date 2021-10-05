87ed4d80bac5bac2aaef42637fbde887ee49af67
Bruno Silva Santana <ibrunoss@outlook.com>
2021-10-04 23:43:20 -0300

    feat: add levelOrder in Outros/BinarySearch.ts

--- Outros/BinarySearch.ts
+++ Outros/BinarySearch.ts
@@ -51,6 +51,26 @@ class BinarySearchTree {
 
     return left > right ? left : right;
   }
+
+  levelOrder(twig: Twig): string {
+    const queue: Twig[] = [twig];
+    const result: number[] = [];
+
+    while (queue.length) {
+      const twig = queue.shift();
+
+      if (!twig) {
+        break;
+      }
+
+      result.push(twig.data);
+
+      twig.left && queue.push(twig.left);
+      twig.right && queue.push(twig.right);
+    }
+
+    return result.join(" ");
+  }
 }
 
 // Testes
@@ -89,6 +109,40 @@ for (let test of getHeight) {
   }
 
   console.log(`expect to be ${test.expect}, received =`, tree.getHeight(twig));
+
+  twig = null;
+}
+
+interface LevelOrderTest {
+  expect: string;
+  data: number[];
+}
+
+const levelOrder: LevelOrderTest[] = [
+  {
+    expect: "3 2 5 1 4 7",
+    data: [3, 5, 4, 7, 2, 1],
+  },
+  {
+    expect: "20 9 50 15 35 62 11 44 13",
+    data: [20, 50, 35, 44, 9, 15, 62, 11, 13],
+  },
+  {
+    expect: "25 12 39 9 19 31 55 23 35 41 60 70 90",
+    data: [25, 39, 12, 19, 9, 23, 55, 31, 60, 35, 41, 70, 90],
+  },
+];
+
+for (let test of levelOrder) {
+  for (let i = 0; i < test.data.length; i++) {
+    twig = tree.insert(twig, test.data[i]);
+  }
+
+  twig &&
+    console.log(
+      `expect to be ${test.expect}, received =`,
+      tree.levelOrder(twig)
+    );
 
   twig = null;
 }
